4082668fb69286113db440ccea92a630c8c3088d
MISTOM <kigardetom2001@gmail.com>
2022-04-08 00:24:18 +0300

    Class Currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,3 +1,16 @@
-export default class Currency{
-  
+/* eslint no-underscore-dangle: 0 */
+export default class Currency {
+  /**
+   *
+   * @param {String} code
+   * @param {String} name
+   */
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
