d144f53782f8a78e5ddd81dd3dcc415bf777eba6
MISTOM <kigardetom2001@gmail.com>
2022-04-07 12:56:20 +0300

    edit resolve

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -5,7 +5,7 @@ export default function getFullResponseFromAPI(success) {
       ? resolve(
         {
           status: 200,
-          body: 'success',
+          body: 'Success',
         },
       )
       : reject(new Error('The fake API is not working currently'));
