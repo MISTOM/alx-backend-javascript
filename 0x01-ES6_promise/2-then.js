d2f3063f2e2bc7689bbc201f4b28c9a247d019ac
MISTOM <kigardetom2001@gmail.com>
2022-04-07 13:29:01 +0300

    fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
-const res = { status: 200, body: 'Success' };
 /**
  *
  * @param {promise} promise
  */
 export default function handleResponseFromAPI(promise) {
+  const res = { status: 200, body: 'Success' };
   promise.then(() => res)
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
