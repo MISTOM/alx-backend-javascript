c034de30bc25b977b233e6aff357e5abc851ffd2
MISTOM <kigardetom2001@gmail.com>
2022-04-07 13:23:55 +0300

    handlers callbacks

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,10 @@
+const res = { status: 200, body: 'Success' };
 /**
  *
  * @param {promise} promise
  */
 export default function handleResponseFromAPI(promise) {
-  promise.then({
-    status: 200,
-    body: 'Success',
-  })
+  promise.then(() => res)
     .catch(() => new Error())
-    .finally(console.log('Got a response from the API'));
+    .finally(() => console.log('Got a response from the API'));
 }
