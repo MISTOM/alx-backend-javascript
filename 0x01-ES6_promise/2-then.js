2f754581cd2e71741259e493e09657514cb80177
MISTOM <kigardetom2001@gmail.com>
2022-04-07 13:31:17 +0300

    return the whole promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,7 +4,8 @@
  */
 export default function handleResponseFromAPI(promise) {
   const res = { status: 200, body: 'Success' };
-  promise.then(() => res)
+  return promise
+    .then(() => res)
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
