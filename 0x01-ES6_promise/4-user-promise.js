77e657ed80d23343fadc1da662d29bd47d5575fd
MISTOM <kigardetom2001@gmail.com>
2022-04-07 14:08:59 +0300

    fix return

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -5,6 +5,5 @@
  * @returns {Promise}
  */
 export default function signUpUser(firstName, lastName) {
-  // eslint-disable-next-line new-cap
-  return new Promise.resolve({ firstName, lastName });
+  return Promise.resolve({ firstName, lastName });
 }
