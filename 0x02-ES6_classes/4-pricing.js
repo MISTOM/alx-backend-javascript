2345f67bb99a06ecd9470b941f27fcd444ecd16b
MISTOM <kigardetom2001@gmail.com>
2022-04-08 00:50:40 +0300

    class Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,59 @@
+import Currency from './3-currency';
+/* eslint no-underscore-dangle: 0 */
+export default class Pricing {
+  /**
+   *
+   * @param {Number} amount
+   * @param {Currency} currency
+   */
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  /**
+   *
+   * @param {Number} amount
+   * @param {Number} conversionRate
+   */
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+
+    return amount * conversionRate;
+  }
+}
