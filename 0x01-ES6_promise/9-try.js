8f479c1d1e4cb68e5faee6f8d52e84424e932642
MISTOM <kigardetom2001@gmail.com>
2022-04-07 18:27:11 +0300

    try catch

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @param {Function} mathFunction
+ * @returns {Array}
+ */
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+    queue.push('Guardrail was processed');
+  } catch (err) {
+    queue.push(err.toString());
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
