0fd8ccd5db486e5e1f297f33ed88d56c67305364
MISTOM <kigardetom2001@gmail.com>
2022-04-07 13:16:22 +0300

    then and finnaly

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {promise} promise
+ */
+export default function handleResponseFromAPI(promise) {
+  promise.then({
+    status: 200,
+    body: 'Success',
+  })
+    .catch(() => new Error())
+    .finally(console.log('Got a response from the API'));
+}
