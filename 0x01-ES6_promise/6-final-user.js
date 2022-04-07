fcac203cbf04e4d6953b59b4955fa03f70b7cd0d
MISTOM <kigardetom2001@gmail.com>
2022-04-07 17:46:41 +0300

    Resolve all

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+/**
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @param {string} fileName
+ */
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.all([user, photo]).then((vals) => {
+    const resArr = [];
+    vals.forEach((val) => {
+      if (val.status === 'fulfilled') {
+        resArr.push({ status: val.status, value: val.value });
+      } else {
+        resArr.push({ status: val.status, value: `Error: ${val.reason.message}` });
+      }
+    });
+    return resArr;
+  });
+}
