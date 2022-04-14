58dd2b10fa85933c7ea937f0da91f7d0e587b681
KIGARDE TOM <75412361+MISTOM@users.noreply.github.com>
2022-04-15 00:16:13 +0300

    Create new Set

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author KIGARDE THOMAS <https://github.com/MISTOM/>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
