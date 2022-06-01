f1f8b20ee3ae4ddc1c08da21d30408c318abcb47
MISTOM <kigardetom2001@gmail.com>
2022-06-01 21:48:54 +0300

    Linting

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
-const http = require('node:http');
+const http = require('http');
 
-const app = http.createServer();
 const PORT = 1245;
 const HOST = 'localhost';
+const app = http.createServer();
 
 app.on('request', (req, res) => {
   const message = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', message.length);
-  res.statusCode = 200
-
+  res.statusCode = 200;
   res.write(Buffer.from(message));
 });
 
 app.listen(PORT, HOST, () => {
-  console.log(`App running on http://${HOST}:${PORT}/`);
+  process.stdout.write(`App running on http://${HOST}:${PORT}`);
 });
-module.exports = app
+module.exports = app;
