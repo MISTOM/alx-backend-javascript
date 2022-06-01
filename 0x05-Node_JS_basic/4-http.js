e036b92114051538fe2a2765b48863fd4f3f75f6
MISTOM <kigardetom2001@gmail.com>
2022-06-01 21:31:08 +0300

    simple http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const http = require('node:http');
+
+const app = http.createServer();
+const PORT = 1245;
+const HOST = 'localhost';
+
+app.on('request', (req, res) => {
+  const message = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', message.length);
+  res.write(Buffer.from(message));
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`App running on http://${HOST}:${PORT}/`);
+});
